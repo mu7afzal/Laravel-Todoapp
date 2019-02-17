0536fb0a994e4c97582b651e4b8c6e49911b521d
Muhammad Usman Afzal <mu7afzal@gmail.com>
2019-02-18 01:04:15 +0500

    auth system complated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ji baba Ji
                 </div>
 
                 <div class="links">
